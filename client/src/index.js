f95c1642671b9051e537cc0d12085c07e95fe823
Nascimento, Eduardo (ESI) <eduardo.nascimento@adp.com>
2020-10-08 20:49:24 -0300

    [master] - switching from material-ui to primereact

--- client/src/index.js
+++ client/src/index.js
@@ -1,47 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+import PrimeReact from 'primereact/utils';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 import { RootContextProvider } from './components/RootContext';
 import PageHeader from './components/PageHeader';
-import PlateLoader from './components/PlateLoader';
-import OrderNow from './components/OrderNow';
+import PageBody from './components/PageBody';
 
 import './index.scss';
 
 function App() {
-    const theme = createMuiTheme({
-        palette: {
-            primary: {
-                main: '#ffecf7',
-                dark: '#ccbac3',
-                light: '#ffffff',
-            },
-            secondary: {
-                main: '#486480',
-                dark: '#1b3a53',
-                light: '#7691af',
-            },
-            success: {
-                main: '#76ff03',
-                dark: '#32cb00',
-                light: '#b0ff57',
-            }
-        },
-    });
-    theme.palette.primary.contrastText = theme.palette.secondary.dark;
-    theme.palette.secondary.contrastText = theme.palette.primary.main;
+    PrimeReact.ripple = true;
 
     return (
-        <ThemeProvider theme={theme}>
-            <RootContextProvider>
-                <div id="material-ui-overload">
-                    <PageHeader />
-                    <PlateLoader />
-                    <OrderNow />
-                </div>
-            </RootContextProvider>
-        </ThemeProvider>
+        <RootContextProvider>
+            <div id="styles-overload">
+                <PageHeader />
+                <PageBody />
+            </div>
+        </RootContextProvider>
     );
 }
 
