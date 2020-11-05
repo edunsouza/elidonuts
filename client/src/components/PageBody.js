7049e5aaae9c3d8c438f924c9fb7eaaa8b6c6c90
Nascimento, Eduardo (ESI) <eduardo.nascimento@adp.com>
2020-11-05 02:54:21 -0300

    [master] - putting back initial animation

--- client/src/components/PageBody.js
+++ client/src/components/PageBody.js
@@ -3,13 +3,9 @@ import PlateLoader from './PlateLoader';
 import OrderNow from './OrderNow';
 
 export default function PageBody() {
-    // const [isLoading, setLoading] = useState(true);
-
-
+    const [isLoading, setLoading] = useState(true);
     // TODO: DELETE
-    const [isLoading, setLoading] = useState(false);
-
-
+    // const [isLoading, setLoading] = useState(false);
     return (
         <>
             {isLoading
